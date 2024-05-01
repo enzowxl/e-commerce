9c630a0c4af937d15ae6460691e0f0c816600b2b
enzowxl <whayyzs13@gmail.com>
2024-05-01 20:17:09 -0300

    feat: add api wrapper and env.ts (checks if all envs were sent correctly)

---
+++ src/env.ts
@@ -0,0 +1,19 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string(),
+    DATABASE_URL: z.string().url(),
+  },
+
+  // client: {
+  // },
+
+  runtimeEnv: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+})
