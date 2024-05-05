9fe9ccd58b76fe84c55ae0e13a8aa841e63657ed
enzowxl <whayyzs13@gmail.com>
2024-05-05 15:58:26 -0300

    feat: organize folders in app, and create layouts, dashboard and shop

---
+++ src/providers/session.tsx
@@ -0,0 +1,6 @@
+import { SessionProvider as NextSessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react'
+
+export function SessionProvider({ children }: { children: ReactNode }) {
+  return <NextSessionProvider>{children}</NextSessionProvider>
+}
