3993d81748684194f17ca3ef82405f09b783f029
enzowxl <whayyzs13@gmail.com>
2024-05-13 22:33:47 -0300

    refactor: ajust cart list and sheet

--- src/app/(shop)/_components/cart/cart-list.tsx
+++ src/app/(shop)/_components/cart/cart-list.tsx
@@ -6,13 +6,13 @@ import { CartItem } from './cart-item'
 export function CartList() {
   const { cart } = useCart()
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 overflow-auto">
       {cart.length > 0 ? (
         cart.map((product) => {
           return <CartItem key={product.id} product={product} />
         })
       ) : (
-        <Label className="text-base">Seu carrinho esta vazio.</Label>
+        <Label className="text-base">Your shopping cart is empty</Label>
       )}
     </div>
   )
--- src/app/(shop)/_components/cart/cart-sheet.tsx
+++ src/app/(shop)/_components/cart/cart-sheet.tsx
@@ -4,16 +4,25 @@ import { CircleX } from 'lucide-react'
 import Link from 'next/link'
 import { ReactNode } from 'react'
 
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
 import {
   Sheet,
   SheetClose,
   SheetContent,
   SheetTrigger,
 } from '@/components/ui/sheet'
+import { useCart } from '@/context/cart'
 
 import { CartList } from './cart-list'
 
 export function CartSheet({ children }: { children: ReactNode }) {
+  const { cart } = useCart()
+
+  function finishOrder() {
+    console.log(cart)
+  }
+
   return (
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
@@ -26,7 +35,20 @@ export function CartSheet({ children }: { children: ReactNode }) {
             <CircleX className="min-w-5 min-h-5 cursor-pointer" />
           </SheetClose>
         </div>
-        <CartList />
+        <div className="flex-1 flex flex-col gap-4 justify-between">
+          <div className="flex flex-col gap-4">
+            <Label className="text-lg font-semibold leading-none tracking-tight pb-5">
+              My cart
+            </Label>
+            <CartList />
+          </div>
+          <Button
+            onClick={finishOrder}
+            className="bg-color-primary text-white h-12 rounded-xl flex gap-2"
+          >
+            Finish order
+          </Button>
+        </div>
       </SheetContent>
     </Sheet>
   )
